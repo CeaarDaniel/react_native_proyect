a2c4597cb03cfb46db3dc17e1492f56b8ab96363
CeaarDaniel <73078916+CeaarDaniel@users.noreply.github.com>
2023-07-26 23:17:04 +0000

    Agregando un tunel

--- react_prueba/App.js
+++ react_prueba/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello word, this is the firs app usinf vscode spaces</Text>
+      <Text>Para poder abrir esta aplicacion creada en vscode spaces hay que agregar
+            un tunel, esto se hace con un comando de la cli de expo
+         </Text>  
+         <Text>
+          el comando usa es: expo start --tunnel
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
